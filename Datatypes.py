937fe1c7227d01e4a259aaac8c2d757869e563a4
krishalcoder123 <krishalsunuwar@gmail.com>
2025-04-29 19:42:43 +0545

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,20 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name :", name)
+print("Data Type of Name is",type (name))
+print("age :", age)
+print("Data Type of age is",type (age))
+print("is_student", is_student)
+print("Data Tpe of is_student is",type (is_student))
+print("weight :", weight)
+print("Data Type of weight id", type(weight))
+
+print("\n After Type Casting ....")
+age= str(age)
+print(age)
+print("Data Type of Age is",(age))
+weight= int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
