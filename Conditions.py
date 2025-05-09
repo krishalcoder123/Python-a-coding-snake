68fdf5f56e8b6c15116882ae21757a5ac694cb75
krishalcoder123 <krishalsunuwar@gmail.com>
2025-05-09 18:37:13 +0545

    Add files via upload

---
+++ Conditions.py
@@ -0,0 +1,11 @@
+number=int(input("Enter Number to check"))
+print("Number to be checked :", number)
+
+if number >0:
+ print("this is a positive number")
+
+elif number<0:
+  print("This is a negative number")
+
+else:
+  print("This is neutral")
