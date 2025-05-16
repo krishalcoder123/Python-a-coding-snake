cc281f406803a9818f74e58846706e6ba5b41b15
krishalcoder123 <krishalsunuwar@gmail.com>
2025-05-16 18:39:02 +0545

    Add files via upload

---
+++ no2functions.py
@@ -0,0 +1,8 @@
+def absolute_value(num) :
+    if num>= 0:
+        return num
+    else:
+        return -num
+    
+print("absolute value of 89 :", absolute_value(89))
+print("absolute value of -189 :", absolute_value(-189))
