572b9bc9a84d4a9ac18411c70ad02a9b68805676
krishalcoder123 <krishalsunuwar@gmail.com>
2025-05-30 18:34:10 +0545

    Add files via upload

---
+++ starpattern.py
@@ -0,0 +1,11 @@
+print("Star Pattern /n")
+for i in range(6,1):
+    for j in range(1):
+        print("*", end="")
+    print("/n")
+
+    print("Inverted Star Pattern \n")
+for i in range(6,1,-1):
+    for j in range(i,1,-1):
+        print("*", end="")
+    print("\n")
